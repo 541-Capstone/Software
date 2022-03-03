2d4841904cfb666c74acdc4a08d64783696d75cb
tsopeter <tsopeter@ku.edu>
2022-03-03 11:10:16 -0600

    Removed setupButtonImages
    Removed setupButtonImages as ImageButtons are now TextButtons

--- Software/Groovebox/Source/Timeline.cpp
+++ Software/Groovebox/Source/Timeline.cpp
@@ -25,9 +25,6 @@ Timeline_t::Timeline_t(int x, int y, int sc, int *numtracks) {
     
     this->currentTrack = nullptr;
     
-    /* setup image buttons , not used anymore */
-    setupButtonImages ();
-    
     audioTrackList = new std::vector<tracktion_engine::Track*>();
     
     /* set that buttons are included in myObjects */
@@ -164,79 +161,7 @@ std::function<void(juce::Graphics*, tracktion_engine::Edit*)> Timeline_t::drawSt
     return paintFunc;
 }
 
+/* do nothing for now */
 void Timeline_t::setupButtonImages() {
     return;
-    /*
-    juce::Colour zeroAlpha = juce::Colour::fromRGBA(0x00,
-                                                    0x00,
-                                                    0x00,
-                                                    0x00);
-    juce::Colour darken = juce::Colour::fromRGBA(0xFF,
-                                                 0x00,
-                                                 0x00,
-                                                 128);
-    
-    const int y_spacing = 20;
-    const int x_spacing = 20;
-    const int leftmost = windowCenter - (1.5 * controlImageWidthpx) - x_spacing;
-    
-    playBtn.setImages(true,
-                   true,
-                   true,
-                   playImage,
-                   100,
-                   zeroAlpha,
-                   playHoverImage,
-                   100,
-                   darken,
-                   playImage,
-                   100,
-                   zeroAlpha);
-
-    
-    playBtn.setBounds(leftmost,
-                   y_spacing,
-                   controlImageWidthpx,
-                   controlImageHeightpx);
-    
-    recordBtn.setImages(true,
-                    true,
-                    true,
-                    recordImage,
-                    100,
-                    zeroAlpha,
-                    recordHoverImage,
-                    100,
-                    zeroAlpha,
-                    recordImage,
-                    100,
-                    zeroAlpha);
-    
-    
-    recordBtn.setBounds(leftmost + x_spacing + controlImageWidthpx,
-                     y_spacing,
-                     controlImageWidthpx,
-                     controlImageHeightpx);
-    
-    pauseBtn.setImages(true,
-                    true,
-                    true,
-                    pauseImage,
-                    100,
-                    zeroAlpha,
-                    pauseHoverImage,
-                    100,
-                    zeroAlpha,
-                    pauseImage,
-                    100,
-                    zeroAlpha);
-    
-    
-    pauseBtn.setBounds(leftmost + (2 * x_spacing) + (2 * controlImageWidthpx),
-                    y_spacing,
-                    controlImageWidthpx,
-                    controlImageHeightpx);
-    
-    */
-    
 }
