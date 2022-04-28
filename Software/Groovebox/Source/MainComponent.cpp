d35bcd7505fbb9c2f6fd7dad1031af4c5480ee0e
tsopeter <tsopeter@ku.edu>
2022-04-28 12:43:47 -0500

    Removed code that isn't being used

--- Software/Groovebox/Source/MainComponent.cpp
+++ Software/Groovebox/Source/MainComponent.cpp
@@ -389,10 +389,6 @@ void MainComponent::disableAllStates(){
 
 /* This assumes that the type of messaegs are of type UNIVERSAL */
 void MainComponent::universalControls(const juce::MidiMessageMetadata &metadata) {
-    /* get the controller Value */
-    const int controllerValue = metadata.getMessage().getControllerValue();
-    
-    //Helpers::ContextualCommands cmd = Helpers::getContextualCmdType(metadata.getMessage());
     
     /* get the message */
     const juce::MidiMessage message = metadata.getMessage();
